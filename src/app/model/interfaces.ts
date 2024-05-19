979a96bc4ad8c2afe5cf5053a348f0d3d66b5d5c
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-19 18:43:39 +0200

    15. Creo la interfaz IRose dentro de model

---
+++ src/app/model/interfaces.ts
@@ -0,0 +1,15 @@
+export interface IRose {
+  id: number;
+  name: string;
+  variety: string;
+  flowerColour: string;
+  flowerScent: string;
+  intensityFragrance: number;
+  flowerSize: number;
+  durabilityFlowers: number;
+  heightCm: number;
+  frostResistanceCelsius: number;
+  diseaseResistance: number;
+  waterRequirements: number;
+  sunExposure: number;
+}
