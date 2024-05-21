fd377875b0a56f10a2b0e2a22cc063e6790f7952
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-21 14:49:07 +0200

    22. Vinculo los elementos del form de la vista de HomeComponent con las variables del controlador de HomeComponent usando ngModel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,6 +15,20 @@ import {HeaderComponent} from "../../shared/components/header/header.component";
 export class HomeComponent implements OnInit {
   title: string | undefined ;
 
+  photo = "";
+  name = "";
+  variety = "";
+  flowerColour = "";
+  flowerScent = "";
+  intensityFragrance = 0;
+  flowerSize = 0;
+  durabilityFlowers = 0;
+  heightCm = 0;
+  frostResistanceCelsius = 0;
+  diseaseResistance = 0;
+  waterRequirements = 0;
+  sunExposure = 0;
+
   constructor() { }
 
   ngOnInit(): void {
