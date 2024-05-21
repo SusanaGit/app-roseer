cac69fc0fb5a54e1c8eccae6b1da98094b4f8fe9
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-21 14:17:04 +0200

    17. Creo el método getRoses() en el servicio roses.service.ts para que devuelva un array de roses

--- src/app/services/roses.service.ts
+++ src/app/services/roses.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {IRose} from "../model/interfaces";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RosesService {
 
-  constructor() { }
+  constructor() {
+    console.log("RosesService initialized")
+  }
+
+  getRoses(): IRose[] {
+
+    const roses = window.localStorage.getItem("roses");
+
+    if (roses) {
+      console.log("Roses isn't null.");
+      const parsedRoses = JSON.parse(roses);
+      return parsedRoses;
+    } else {
+      console.log("Roses is null.")
+      return [];
+    }
+  }
+
 }
